3f488a679eeaba38c3e1bf429854e48f68b5a316
ArilisDev <32502295+Arilis@users.noreply.github.com>
2020-08-12 01:37:29 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    Token: `NTc0NDk1NTk4NjY1OTI0NjI5.XuDTTw.I8UJdLPn6Q4NCiRLdqVoIAIjrjA`,
-    AntiCaptchaAPIKey = `288dd4a87117dd6773bd952f4b3dd96f`,
+    Token: ``,
+    AntiCaptchaAPIKey = ``,
     Version = `v0.1`
 }
